7e0ed0ac6681f590df2625398429e590dda106ff
Cxlopez <cristian4402@hotmail.com>
2024-05-21 20:28:14 -0400

    create all categories

--- src/App.js
+++ src/App.js
@@ -10,6 +10,41 @@ function App() {
           <p>Shop Now</p>
         </div>
       </div>
+      <div className='category-container'>
+        {/* <img src="" alt="" /> */}
+        <div className='category-body-container'>
+          <h2>Hats</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className='category-container'>
+        {/* <img src="" alt="" /> */}
+        <div className='category-body-container'>
+          <h2>Jackets</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className='category-container'>
+        {/* <img src="" alt="" /> */}
+        <div className='category-body-container'>
+          <h2>Sneakers</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className='category-container'>
+        {/* <img src="" alt="" /> */}
+        <div className='category-body-container'>
+          <h2>Womans</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className='category-container'>
+        {/* <img src="" alt="" /> */}
+        <div className='category-body-container'>
+          <h2>Mens</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
     </div>
   );
 }
