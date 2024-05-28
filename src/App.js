0934625e1948880f81b41b3258887b6d23f25c01
Cxlopez <cristian4402@hotmail.com>
2024-05-27 20:05:56 -0400

    add styling to app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./categories.styles.scss";
 
 function App() {
   const categories = [
@@ -13,7 +13,7 @@ function App() {
     <div className="categories-container">
       {categories.map(({ title }) => (
       <div className="category-container">
-        {/* <img src="" alt="" /> */}
+        <div className='background-image' />
         <div className="category-body-container">
           <h2>{title}</h2>
           <p>Shop Now</p>
